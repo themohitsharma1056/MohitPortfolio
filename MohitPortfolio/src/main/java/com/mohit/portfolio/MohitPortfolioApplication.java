1859814b4df6f417874a47cf07e80fbae7c880ce
mohit sharma <the.mohit1056@gmail.com>
2026-05-17 20:17:03 +0530

    Initial Spring Boot portfolio setup

---
+++ MohitPortfolio/src/main/java/com/mohit/portfolio/MohitPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohitPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohitPortfolioApplication.class, args);
+	}
+
+}
---
+++ MohitPortfolio/src/test/java/com/mohit/portfolio/MohitPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohitPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
