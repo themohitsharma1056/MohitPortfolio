953ea354b9046dae12282ec63c206928528b310c
mohit sharma <the.mohit1056@gmail.com>
2026-06-03 18:16:31 +0530

    fix login authentication for aws server

--- MohitPortfolio/src/main/java/com/mohit/portfolio/config/SecurityConfig.java
+++ MohitPortfolio/src/main/java/com/mohit/portfolio/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.mohit.portfolio.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -9,7 +14,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+    @Value("${ADMIN_USERNAME}")
+    private String adminUsername;
 
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+	
+    @Bean
+		public UserDetailsService users() {
+	
+
+		    UserDetails admin = User
+		            .withUsername(adminUsername)
+		            .password("{noop}" + adminPassword)
+		            .roles("ADMIN")
+		            .build();
+
+		    return new InMemoryUserDetailsManager(admin);
+		}
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(
 	        HttpSecurity http
